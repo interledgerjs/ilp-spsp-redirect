834bd9852528d5fb7c0910d6fc20fedccb20b28e
Ben Sharafian <benms64@gmail.com>
2018-04-05 11:25:37 -0700

    fix: make it work

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-zsh:1: command not found: :w
+const makePlugin = require('ilp-plugin')
 const crypto = require('crypto')
 const SPSP = require('ilp-protocol-spsp')
 const IlpStream = require('ilp-protocol-stream')
+const EventEmitter = require('eventemitter2')
 
 const base64url = buf => buf
   .toString('base64')
   .replace(/\//g, '_')
-  .replace(/+/g, '-')
+  .replace(/\+/g, '-')
   .replace(/=/g, '')
 
-class IlpRedirect {
+class IlpRedirect extends EventEmitter {
   constructor (opts) {
+    super()
+
     this.makePlugin = (opts && opts.makePlugin) || makePlugin
     this.connected = false
     this.server = null
@@ -25,7 +28,7 @@ class IlpRedirect {
     await plugin.connect()
 
     const { destinationAccount, sharedSecret } = await SPSP.query(receiver)
-    const conn = IlpStream.createConnection({
+    const conn = await IlpStream.createConnection({
       plugin,
       destinationAccount,
       sharedSecret
@@ -46,15 +49,16 @@ class IlpRedirect {
       serverSecret: crypto.randomBytes(32)
     })
 
-    server.on('connection', conn => {
+    this.server.on('connection', conn => {
       const receiver = Buffer.from(conn.connectionTag, 'base64').toString('utf8')
 
       conn.on('money_stream', incomingStream => {
         incomingStream.setReceiveMax(Infinity)
-        incomingStream.on('incoming', amount => {
+        incomingStream.on('incoming', async amount => {
           const outgoingStream = await this._getStream(receiver)
-          const newAmount = outgoingStream.getSendMax() + amount
+          const newAmount = Number(outgoingStream.sendMax) + Number(amount)
           outgoingStream.setSendMax(newAmount)
+          this.emit('receiver:' + receiver, String(newAmount))
         })
       })
     })
@@ -62,9 +66,20 @@ class IlpRedirect {
     await this.server.listen()
   }
 
-  async response (receiver) {
+  response (receiver) {
     // encode the spsp receiver so that it fits in the ILP address
     const encodedReceiver = base64url(Buffer.from(receiver, 'utf8'))
-    return this.server.getAddressAndSecret(encodedReceiver)
+    const response = this.server.generateAddressAndSecret(encodedReceiver)
+    return {
+      destination_account: response.destinationAccount,
+      shared_secret: response.sharedSecret.toString('base64')
+    }
+  }
+
+  sent (receiver) {
+    const stream = this.streams.get(receiver)
+    return (stream && stream.sendMax) || '0'
   }
 }
+
+module.exports = IlpRedirect
